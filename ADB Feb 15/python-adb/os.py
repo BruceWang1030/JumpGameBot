76f427ad397385b2ca976437e3e3d8104b1aeda2
Bruce Wang <brucedebugstheworld@users.noreply.github.com>
2019-01-16 17:07:59 -0500

    delete repeated files

--- ADB Feb 15/python-adb/os.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system('adb version')
-
-os.system('adb shell screencap -p /sdcard/adbscreenshot/screenshot.png')
-os.system('adb pull /sdcard/adbscreenshot/screenshot.png C:\Bruce\screenshotsaver\screenshot.png')
-os.system('adb shell rm /sdcard/adbscreenshot/screenshot.png')
